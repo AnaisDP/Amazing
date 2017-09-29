b71ff7a07cec8ce7d4d509f8a283393aa28f82bd
tomapter <tom@eccmperformance.com>
2017-09-29 13:40:50 +0200

    commit tom maven

---
+++ maventomTest/src/main/java/testLog4j.java
@@ -0,0 +1,11 @@
+/*import org.apache.log4j.Logger;
+
+
+public class testLog4j {
+        private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+        public static void main(String[] args) {
+            LOGGER.error("Hello World !");
+        }
+    }
+}*/
