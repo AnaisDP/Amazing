c7222b4ea71d4a9bcb91440ab82099d4b8198c28
OussamaAyoub <32196617+OussamaAyoub@users.noreply.github.com>
2017-10-13 16:15:18 +0200

    Creation Elf

---
+++ CardGames/src/main/java/Model/Elf.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Elf extends Race {
+    public Elf(){
+        this.name="Elf";
+        this.power="Copy and use the power of one of the card in front of you";
+    }
+    void Power(){
+
+    }
+}
--- CardGames/src/main/java/Model/Gnome.java
+++ CardGames/src/main/java/Model/Gnome.java
@@ -7,6 +7,6 @@ public class Gnome extends Race {
 
     }
     void Power(){
-        
+
     }
 }
