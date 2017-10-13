c3035a958e17938c2b91d66f97e8a7ec21c335db
tomapter <tom@eccmperformance.com>
2017-10-13 15:54:36 +0200

    Add Troll

---
+++ CardGames/src/main/java/Model/Troll.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Troll extends Race{
+
+    public Troll(){
+        this.name="Troll";
+        this.power = "Swap the cards in front of you with the cards in front of your opponent";
+    }
+
+}
