da493dd11dbb57885b3616f0589fdcecb6e0e1b7
tomapter <tom@eccmperformance.com>
2017-10-13 15:18:22 +0200

    Ajout classe mere Card

---
+++ CardGames/src/main/java/M_Carte.java
@@ -0,0 +1,15 @@
+public class M_Carte {
+    private int id;
+    private String power;
+    private Boolean faceDown;
+    private String name;
+
+    public M_Carte(int id, String power, Boolean faceDown, String name){
+        this.id = id;
+        this.power = power;
+        this.faceDown = faceDown;
+        this.name= name;
+    }
+
+
+}
--- CardGames/src/main/java/M_Troll.java
+++ CardGames/src/main/java/M_Troll.java
@@ -1,14 +1,3 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
+public class M_Troll {
 
-public class M_Troll extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }
