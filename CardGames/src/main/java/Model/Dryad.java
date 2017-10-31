15c7c797508f1764670cca37bf3d5cd81c48fb6a
OussamaAyoub <32196617+OussamaAyoub@users.noreply.github.com>
2017-10-31 17:06:32 +0100

    dryad + gnome

--- CardGames/src/main/java/Model/Dryad.java
+++ CardGames/src/main/java/Model/Dryad.java
@@ -3,7 +3,7 @@ package Model;
 public class Dryad extends Race {
     public Dryad(){
         this.name = "Dryad";
-        this.power = "stole a card in front of your opponent and add it in front of you without activating its power.";
+        this.power = "Stole a card in front of your opponent and add it in front of you without activating its power.";
     }
 
     public void power(){
--- CardGames/src/main/java/Model/Gnome.java
+++ CardGames/src/main/java/Model/Gnome.java
@@ -3,7 +3,7 @@ package Model;
 public class Gnome extends Race {
     public Gnome(){
         this.name = "Troll";
-        this.power = "draw 2 cards";
+        this.power = "Draw 2 cards";
     }
 
     public void power(){
