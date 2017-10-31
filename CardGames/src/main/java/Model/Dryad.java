3a91a93a52c266624ec6112216b965697ebd9766
OussamaAyoub <32196617+OussamaAyoub@users.noreply.github.com>
2017-10-31 17:00:43 +0100

    dryad + gnome

---
+++ CardGames/src/main/java/Model/Dryad.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Dryad extends Race {
+    public Dryad(){
+        this.name = "Dryad";
+        this.power = "stole a card in front of your opponent and add it in front of you without activating its power.";
+    }
+
+    public void power(){
+
+    }
+}
