200fc4e4ac2f26f748bd20269591159a609975c0
tomapter <tom@eccmperformance.com>
2017-10-31 22:51:48 +0100

    Boarde controller init and start function

--- CardGames/src/main/java/Controller/BoardController.java
+++ CardGames/src/main/java/Controller/BoardController.java
@@ -1,4 +1,24 @@
 package Controller;
 
-public class BoardController {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Stack;
+
+public class BoardController extends Application{
+    private Stack<CarteController> deck;
+
+    public BoardController(){
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
 }
