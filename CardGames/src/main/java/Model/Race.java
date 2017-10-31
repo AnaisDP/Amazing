617db4b942c46631aeb67ca7a31d283b17f8788d
tomapter <tom@eccmperformance.com>
2017-10-31 15:24:44 +0100

    first commit

---
+++ CardGames/src/main/java/Model/Race.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Race {
+    protected String name;
+    protected String image;
+    protected String power;
+}
---
+++ CardGames/src/main/java/Model/Troll.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class Troll extends Race {
+    public Troll(){
+        this.name = "Troll";
+        this.power = "swipe the hand of your opponent with yours";
+    }
+}
