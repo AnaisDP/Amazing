ea8ca07c03a8daed289206ea14ebb52a1e6d5e7c
benyaya225 <dao_benyaya@hotmail.fr>
2017-11-08 12:25:11 +0100

    gobelin and troll power

--- src/main/java/com/amazing/software/Model/Gobelin.java
+++ src/main/java/com/amazing/software/Model/Gobelin.java
@@ -1,8 +1,24 @@
 package com.amazing.software.Model;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import static javafx.scene.input.KeyCode.L;
+
 public class Gobelin extends Race {
     public Gobelin(){
         this.name = "Gobelin";
         this.power = "Switch your hand with you opponent";
+    }
+
+    public void Power(Joueur joueur1, Joueur joueur2)
+    {
+        List<Carte> tmp;
+        tmp = joueur1.getMain();
+        joueur1.setMain(joueur2.getMain());
+        joueur2.setMain(tmp);
+
     }
 }
--- src/test/resources/com.amazing.software/features/Conjured.feature
+++ src/test/resources/com.amazing.software/features/Conjured.feature
@@ -5,3 +5,12 @@ Scenario: Create Game
   Then each players have 0 cards
   When I Create a new Game
   Then each players have 5 cards
+
+Scenario: Draw Card
+  Given I have a player
+  Then He have 0 card
+  When He draw 1 card
+  Then He have 1 card
+
+Scenario: Gobelin Power*
+  Given
