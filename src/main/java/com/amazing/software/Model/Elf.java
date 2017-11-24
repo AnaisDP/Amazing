26a2dabb1534da080bee260ca8fff9bd7895c570
jeang <jeansh.guillaume@gmail.com>
2017-11-24 15:26:29 +0100

    Removed .idea and changed .gitignore

--- src/main/java/com/amazing/software/Model/Elf.java
+++ src/main/java/com/amazing/software/Model/Elf.java
@@ -1,6 +1,7 @@
 package com.amazing.software.Model;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Elf extends Race {
@@ -10,9 +11,16 @@ public class Elf extends Race {
         this.name = "Elf";
         this.power = "Copy and use the power of one of the card in front of you";
     }
+
     @Override
-    public void Power(Player p1,Player p2, Stack<Card> Deck, int index){
-        Card card=p1.getBoard().get(index);
-        card.getRace().Power(p1,p2,Deck,index);
+    public void Power(Player p1, Player p2, Stack<Card> Deck, int index) {
+        if(p1.getBoard().size()==1){
+            Scanner reader = new Scanner(System.in);
+            System.out.println("Quelle carte de votre board choisissez vous de copier ?");
+            int cardchosen = reader.nextInt();
+            p1.getBoard().get(cardchosen).getRace().Power(p1, p2, Deck, index);
+        }
+
+
     }
 }
